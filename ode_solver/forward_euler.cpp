f1a8bd7a9a0d6fee65ed4839806cc792a1241207
Lucas <lucas@evidentinsights.com>
2026-03-14 21:59:10 +0000

    Added PyBind wrapper

--- ode_solver/forward_euler.cpp
+++ ode_solver/forward_euler.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
+#include <pybind11/pybind11.h>
 
 int forward_euler(float (*function)(float, float), float y0, float step, float t_final, std::string filename) {
     std::vector<float> times = {0};
@@ -10,26 +11,26 @@ int forward_euler(float (*function)(float, float), float y0, float step, float t
     float t = 0;
     float y = y0;
 
-    while (t < t_final) {
-        float dy = function(t, y);
-        t += step;
-        y += dy * step;
-
-        times.push_back(t);
-        y_values.push_back(y);
-    }
-
     std::ofstream outFile(filename);
 
     if (outFile.is_open()) {
         outFile << "t,y\n";
 
-        for (int i = 0; i < times.size(); i++){
-            outFile << times[i] << "," << y_values[i] << "\n";
+        while (t < t_final) {
+            float dy = function(t, y);
+            t += step;
+            y += dy * step;
+
+            outFile << t << "," << y << "\n";
         }
 
         outFile.close();
     }
     
     return 0;
+}
+
+PYBIND11_MODULE(ode_solver, m) {
+    m.doc() = "A module for solving ODEs.";
+    m.def("forward_euler", &forward_euler, "A forward Euler ODE solver");
 }
