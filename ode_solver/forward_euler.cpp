619b0731ab8c17388d551b17c04faacb0b1f8c55
Lucas <lucas@evidentinsights.com>
2026-03-15 11:47:14 +0000

    Adding SEIR functions

--- ode_solver/forward_euler.cpp
+++ ode_solver/forward_euler.cpp
@@ -5,21 +5,39 @@
 
 namespace py = pybind11;
 
-int forward_euler(std::function<float(float, float)>& function, float y0, float step, float t_final, const std::string& filename) {
-    float t = 0;
-    float y = y0;
+float ds_dt(float i, float s, float beta) {
+    return -beta * i * s;
+}
+float de_dt(float i, float s, float beta, float sigma, float e) {
+    return beta * i * s - sigma * e;
+}
+float di_dt(float i, float sigma, float e, float gamma) {
+    return sigma * e - gamma * i;
+}
+float dr_dt(float i, float gamma) {
+    return gamma * i;
+}
+
+int forward_euler(float beta, float sigma, float gamma, float s_0, float e_0, float i_0, float r_0, float step, float t_final, const std::string& filename) {
+    float t = 0.0, s = s_0, e = e_0, i = i_0, r = r_0;
 
     std::ofstream outFile(filename);
 
     if (outFile.is_open()) {
-        outFile << "t,y\n";
+        outFile << "time,susceptible,exposed,infected,recovered\n";
 
         while (t < t_final) {
-            float dy = function(t, y);
+            float ds = ds_dt(i, s, beta);
+            s += ds * step;
+            float de = de_dt(i, s, beta, sigma, e);
+            e += de * step;
+            float di = di_dt(i, sigma, e, gamma);
+            i += di * step;
+            float dr = dr_dt(i, gamma);
+            r += dr * step;
+        
+            outFile << t << "," << s << "," << e << "," << i << "," << r <<"\n";
             t += step;
-            y += dy * step;
-
-            outFile << t << "," << y << "\n";
         }
 
         outFile.close();
