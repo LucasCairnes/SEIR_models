e7276bcfd8272d6dda39111d451fc8e78186f6fa
Lucas <lucas@evidentinsights.com>
2026-03-19 14:15:29 +0000

    Implemented movement and boundary check logic

--- seri_monte_carlo.cpp
+++ seri_monte_carlo.cpp
@@ -22,12 +22,14 @@ class System{
         System(int length) : side_length(length), unii(0, length - 1) {
             lattice.resize(length, std::vector<int>(length, 0));
         }
-        
+
         int get_random_index() {
             return unii(rng);
         };
 
         void populate_lattice(int agent_count, float susceptible);
+        int boundary_check(int coord);
+        void move_agent(Agent agent, int x_destination, int y_destination);
 };
 
 void System::populate_lattice(int agent_count, float susceptible) {
@@ -48,5 +50,24 @@ void System::populate_lattice(int agent_count, float susceptible) {
 
         lattice[x_val][y_val] = state;
         agents.push_back(Agent(x_val, y_val, state));
+    }
+};
+
+int System::boundary_check(int coord){
+    if (coord < 0) return side_length - 1;
+    if (coord >= side_length) return 0;
+    return coord;
+}
+
+void System::move_agent(Agent agent, int x_destination, int y_destination) {
+    int x_destination = boundary_check(x_destination);
+    int y_destination = boundary_check(y_destination);
+
+    if (lattice[x_destination][y_destination] != 0) {
+        lattice[agent.x_pos][agent.y_pos] = 0;
+        lattice[x_destination][y_destination] = agent.state;
+    
+        agent.x_pos = x_destination;
+        agent.y_pos = y_destination;
     }
 };
