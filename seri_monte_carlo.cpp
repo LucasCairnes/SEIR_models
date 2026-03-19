739d16ffd3ddb9d8bd6cabbd7e5733a7f85d9d60
Lucas <lucas@evidentinsights.com>
2026-03-19 10:17:28 +0000

    Initialising Monte Carlo C++ File

---
+++ seri_monte_carlo.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+
+class lattice{
+    public:
+        // Parameters
+        int side_length;
+        vector<vector<int>> grid = 
+
+
+
+    private:
+}
+
+class agent{
+
+}
