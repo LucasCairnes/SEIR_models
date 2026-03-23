874b470b118101581f3f4aede7b700bd55d52ba6
Lucas <lucas@evidentinsights.com>
2026-03-23 19:40:18 +0900

    Shifted lattice to be a 2D vector of pointers. Created initialisation functions.

--- seri_monte_carlo.cpp
+++ seri_monte_carlo.cpp
@@ -1,57 +1,61 @@
 #include <vector>
 #include <random>
 #include <cmath>
-using namespace std;
 
-class Agent {
-    public:
-        int x_pos, y_pos, state;
-        Agent(int x, int y, int s)
-            : x_pos(x), y_pos(y), state(s) {}
+struct Agent {
+    int x_pos, y_pos, state;
+    Agent(int x, int y, int s)
+        : x_pos(x), y_pos(y), state(s) {}
 };
 
 class System{
     private:
-        int side_length;
-        vector<vector<int>> lattice;
-        mt19937 rng {42};
-        uniform_int_distribution<int> unii;
-        vector<Agent> agents;
+        int side_length, agent_count, MCS;
+        int susceptible, exposed, infected, recovered;
+        std::vector<std::vector<Agent *>> lattice;
+        std::mt19937 rng {42};
+        std::uniform_int_distribution<int> unii;
+        std::vector<Agent> agents;
+        
+        int get_random_index();
+        void populate_lattice(int agent_count, float s, float e, float i);
+        int boundary_check(int coord);
+        void move_agent(Agent &agent, int x_destination, int y_destination);
 
     public:
-        System(int length) : side_length(length), unii(0, length - 1) {
-            lattice.resize(length, std::vector<int>(length, 0));
+        System(int side_length, int agent_count, int MCS, float s, float e, float i, float r)
+            : side_length(side_length), agent_count(agent_count), MCS(MCS), unii(0, side_length - 1) {
+            lattice.resize(side_length, std::vector<Agent*>(side_length, nullptr));
         }
-
-        int get_random_index() {
-            return unii(rng);
-        };
-
-        void populate_lattice(int agent_count, float susceptible);
-        int boundary_check(int coord);
-        void move_agent(Agent agent, int x_destination, int y_destination);
 };
 
-void System::populate_lattice(int agent_count, float susceptible) {
+int System::get_random_index(){
+    return unii(rng);
+}
+
+void System::populate_lattice(int agent_count, float s, float e, float i) {
     int x_val, y_val, state;
-    int susceptible_count = static_cast<int>(std::round(agent_count * susceptible));
+    susceptible = static_cast<int>(std::round(agent_count * s));
+    exposed = static_cast<int>(std::round(agent_count * e));
+    infected = static_cast<int>(std::round(agent_count * i));
+    recovered = agent_count - susceptible - exposed - infected;
 
-    for (int i = 0; i < side_length; i++) {
-        if (i < susceptible_count) {
-            state = 1;
-        } else {
-            state = 2;
-        };
+    agents.reserve(agent_count);
 
+    for (int j = 0; j < agent_count; j++) {
+        if (j < susceptible) state = 1;
+        else if (j < susceptible + exposed) state = 2;
+        else if (j < susceptible + exposed + infected) state = 3;
+        else state = 4;
         do {
-            int x_val = get_random_index();
-            int y_val = get_random_index();
+            x_val = get_random_index();
+            y_val = get_random_index();
         } while (lattice[x_val][y_val] != 0);
 
-        lattice[x_val][y_val] = state;
-        agents.push_back(Agent(x_val, y_val, state));
+        agents.emplace_back(x_val, y_val, state);
+        lattice[x_val][y_val] = &agents.back();
     }
-};
+}
 
 int System::boundary_check(int coord){
     if (coord < 0) return side_length - 1;
@@ -59,15 +63,15 @@ int System::boundary_check(int coord){
     return coord;
 }
 
-void System::move_agent(Agent agent, int x_destination, int y_destination) {
-    int x_destination = boundary_check(x_destination);
-    int y_destination = boundary_check(y_destination);
+void System::move_agent(Agent &agent, int x_destination, int y_destination) {
+    x_destination = boundary_check(x_destination);
+    y_destination = boundary_check(y_destination);
 
-    if (lattice[x_destination][y_destination] != 0) {
-        lattice[agent.x_pos][agent.y_pos] = 0;
-        lattice[x_destination][y_destination] = agent.state;
+    if (lattice[x_destination][y_destination] == nullptr) {
+        lattice[x_destination][y_destination] = lattice[agent.x_pos][agent.y_pos];
+        lattice[agent.x_pos][agent.y_pos] = nullptr;
     
         agent.x_pos = x_destination;
         agent.y_pos = y_destination;
     }
-};
+}
