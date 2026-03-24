de6254a526ee0859776cae6cf21ab36305caa208
Lucas <lucas@evidentinsights.com>
2026-03-24 13:59:24 +0900

    Added comments to forward euler C++ script

--- seir_forward_euler.cpp
+++ seir_forward_euler.cpp
@@ -3,7 +3,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/functional.h>
 
-namespace py = pybind11;
+namespace py = pybind11; // To condense the pybind args
 
 float ds_dt(float i, float s, float beta) {
     return -beta * i * s;
@@ -16,7 +16,7 @@ float di_dt(float i, float sigma, float e, float gamma) {
 }
 float dr_dt(float i, float gamma) {
     return gamma * i;
-}
+} // The seir differential equations as easily callable functions
 
 int seir_forward_euler(float beta, float sigma, float gamma, float s_0, float e_0, float i_0, float r_0, float step, float t_final, const std::string& filename) {
     float t = 0.0, s = s_0, e = e_0, i = i_0, r = r_0;
@@ -25,13 +25,13 @@ int seir_forward_euler(float beta, float sigma, float gamma, float s_0, float e_
 
     if (outFile.is_open()) {
         outFile << "time,susceptible,exposed,infected,recovered\n";
-        outFile << t << "," << s << "," << e << "," << i << "," << r <<"\n";
+        outFile << t << "," << s << "," << e << "," << i << "," << r <<"\n"; // Writing intial values
 
         while (t < t_final) {
             float ds = ds_dt(i, s, beta);
             float de = de_dt(i, s, beta, sigma, e);
             float di = di_dt(i, sigma, e, gamma);
-            float dr = dr_dt(i, gamma);
+            float dr = dr_dt(i, gamma); // Ensuring to pass the seir variables in before updating them
 
             s += ds * step;
             e += de * step;
@@ -50,7 +50,7 @@ int seir_forward_euler(float beta, float sigma, float gamma, float s_0, float e_
 
 PYBIND11_MODULE(seir_forward_euler, m) {
     m.doc() = "A module for solving SEIR ODEs.";
-    m.def("forward_euler", &seir_forward_euler, "A function to solve the SEIR ODEs with the forward Euler method.",
+    m.def("forward_euler", &seir_forward_euler, "A function to solve the SEIR ODEs with the forward Euler method.", // Brief descriptions of the module and function
         py::arg("beta"),
         py::arg("sigma"),
         py::arg("gamma"),
@@ -60,6 +60,6 @@ PYBIND11_MODULE(seir_forward_euler, m) {
         py::arg("r_0"),
         py::arg("step"),
         py::arg("t_final"),
-        py::arg("filename")
+        py::arg("filename") // Defining pybind args to allow args to be easily using in python, e.g. beta = 1.0
     );
 }
