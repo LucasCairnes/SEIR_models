309e3d5c646d77e9de58bb31f3a429868f32ed59
Lucas <lucas@evidentinsights.com>
2026-03-24 11:26:15 +0900

    Created main update loop. Added comments for clarity

--- seir_monte_carlo.cpp
+++ seir_monte_carlo.cpp
@@ -1,53 +1,71 @@
 #include <vector>
 #include <random>
 #include <cmath>
+#include <algorithm>
+#include <fstream>
+#include <string>
 
 struct Agent {
     int x_pos, y_pos, state;
     Agent(int x, int y, int s)
         : x_pos(x), y_pos(y), state(s) {}
-};
+}; // Using a struct since all variables are public
 
 class System{
     private:
-        int side_length, agent_count, MCS;
+        int length;
         int susceptible, exposed, infected, recovered;
-        std::vector<std::vector<Agent *>> lattice;
+        float beta, sigma, gamma; // Storing variables which will be referenced by various functions
         std::mt19937 rng {42};
         std::uniform_int_distribution<int> creation_rng;
         std::uniform_int_distribution<int> movement_rng;
+        std::uniform_int_distribution<int> state_rng; // Initialising root engine and more specific rngs
+        std::vector<int> lattice; // 1D faster than 2D. Integers instead of pointers so the agents vector can be shuffled to eliminate biases
         std::vector<Agent> agents;
         
-        int get_random_index();
-        int get_random_movement();
-        void populate_lattice(int agent_count, float s, float e, float i);
+        int get_index(int x_value, int y_value);
+        int random_index();
+        int random_movement();
+        int random_state();
+        void populate_lattice(int agent_count, float s_0, float e_0, float i_0, float r_0);
         int boundary_check(int coord);
         void move_agent(Agent &agent, int x_destination, int y_destination);
-        void run_sim(int MCS);
+        void run_sim(int MCS, const std::string& filename);
+        void update_state(Agent &agent); // Initialising all functions, ensuring to pass references to the agent objects
 
     public:
-        System(int side_length, int agent_count, int MCS, float s, float e, float i, float r)
-            : side_length(side_length), agent_count(agent_count), MCS(MCS), creation_rng(0, side_length - 1), movement_rng(-1, 1) {
-            lattice.resize(side_length, std::vector<Agent*>(side_length, nullptr));
+        System(int length, int agent_count, int MCS, float s_0, float e_0, float i_0, float r_0, float beta, float sigma, float gamma, const std::string& filename)
+            : length(length), creation_rng(0, length - 1), movement_rng(-1, 1), state_rng(0, 1), beta(beta), sigma(sigma), gamma(gamma) { // Outlining the required inputs and storing the frequently used variables
+            lattice.resize(length*length, 0);
+            populate_lattice(agent_count, s_0, e_0, i_0, r_0);
+            run_sim(MCS, filename); // Calling the main functions to initialise and run the sim
         }
 };
 
-int System::get_random_index() {
+int System::random_index() {
     return creation_rng(rng);
 }
 
-int System::get_random_movement() {
+int System::random_movement() {
     return movement_rng(rng);
 }
 
-void System::populate_lattice(int agent_count, float s, float e, float i) {
-    int x_val, y_val, state;
-    susceptible = static_cast<int>(std::round(agent_count * s));
-    exposed = static_cast<int>(std::round(agent_count * e));
-    infected = static_cast<int>(std::round(agent_count * i));
-    recovered = agent_count - susceptible - exposed - infected;
+int System::random_state() {
+    return state_rng(rng); // Functionalising the random elements of the sim. Making the state function to work for all states
+}
 
-    agents.reserve(agent_count);
+int System::get_index(int x_value, int y_value) {
+    return (x_value * length) + y_value;
+} // Function to convert 2D -> 1D
+
+void System::populate_lattice(int agent_count, float s_0, float e_0, float i_0, float r_0) {
+    int x_val, y_val, state;
+    susceptible = static_cast<int>(std::round(agent_count * s_0));
+    exposed = static_cast<int>(std::round(agent_count * e_0));
+    infected = static_cast<int>(std::round(agent_count * i_0));
+    recovered = agent_count - susceptible - exposed - infected; // Using left over agents to avoid rounding errors
+
+    agents.reserve(agent_count); // JUSTIFY THIS
 
     for (int j = 0; j < agent_count; j++) {
         if (j < susceptible) state = 1;
@@ -55,38 +73,83 @@ void System::populate_lattice(int agent_count, float s, float e, float i) {
         else if (j < susceptible + exposed + infected) state = 3;
         else state = 4;
         do {
-            x_val = get_random_index();
-            y_val = get_random_index();
-        } while (lattice[x_val][y_val] != 0);
+            x_val = random_index();
+            y_val = random_index();
+        } while (lattice[get_index(x_val, y_val)] != 0); // Checks until it finds an empty index
 
         agents.emplace_back(x_val, y_val, state);
-        lattice[x_val][y_val] = &agents.back();
+        lattice[get_index(x_val, y_val)] = state; // Updating agents vector and lattice
     }
 }
 
 int System::boundary_check(int coord){
-    if (coord < 0) return side_length - 1;
-    if (coord >= side_length) return 0;
-    return coord;
+    if (coord < 0) return length - 1;
+    if (coord >= length) return 0;
+    return coord; // Loops index if exceeding boundary
 }
 
 void System::move_agent(Agent &agent, int x_destination, int y_destination) {
     x_destination = boundary_check(x_destination);
     y_destination = boundary_check(y_destination);
 
-    if (lattice[x_destination][y_destination] == nullptr) {
-        lattice[x_destination][y_destination] = lattice[agent.x_pos][agent.y_pos];
-        lattice[agent.x_pos][agent.y_pos] = nullptr;
+    if (lattice[get_index(x_destination, y_destination)] == 0) {
+        lattice[get_index(x_destination, y_destination)] = lattice[get_index(agent.x_pos, agent.y_pos)];
+        lattice[get_index(agent.x_pos, agent.y_pos)] = 0; // Updates position of the agent and then empties previous location
     
         agent.x_pos = x_destination;
         agent.y_pos = y_destination;
     }
 }
 
-void System::run_sim(int MCS) {
-    for (int k = 0; k < MCS; k++) {
-        for (Agent& curr_agent : agents) {
-            move_agent(curr_agent, curr_agent.x_pos + get_random_movement(), curr_agent.y_pos + get_random_movement());
+void System::update_state(Agent &agent) {
+    int current_state = agent.state;
+
+    if (current_state == 4) return;
+    
+    int infected_neighbours = 0, state_change = 0;
+    int agent_x = agent.x_pos, agent_y = agent.y_pos;
+
+    if (current_state == 1) {
+        if (lattice[get_index(boundary_check(agent_x + 1), agent_y)] == 3) infected_neighbours++;
+        if (lattice[get_index(boundary_check(agent_x -1), agent_y)] == 3) infected_neighbours++;
+        if (lattice[get_index(boundary_check(agent_x), agent_y + 1)] == 3) infected_neighbours++;
+        if (lattice[get_index(boundary_check(agent_x), agent_y - 1)] == 3) infected_neighbours++; // Counts neighbours
+
+        if (infected_neighbours == 0) return;
+
+        state_change = random_state(beta * infected_neighbours);
+    }
+    
+    else {
+        if (current_state == 2) state_change = random_state(sigma);
+        else if (current_state == 3) state_change = random_state(gamma);
+    }
+
+    agent.state = current_state + state_change; // If the rng returns an update, the agents state will increase by 1, otherwise will remain the same
+}
+
+void System::run_sim(int MCS, const std::string& filename) {
+    int step = 0;
+    std::ofstream outFile(filename);
+
+    if (outFile.is_open()) {
+        outFile << "Monte Carlo step,susceptible,exposed,infected,recovered\n";
+        outFile << step << "," << susceptible << "," << exposed << "," << infected << "," << recovered <<"\n";
+
+        for (int k = 0; k < MCS; k++) {
+            std::shuffle(agents.begin(), agents.end(), rng); // Shuffles to eliminate biases to the first agents in the vector
+            for (Agent& curr_agent : agents) {
+                move_agent(curr_agent, curr_agent.x_pos + random_movement(), curr_agent.y_pos + random_movement());
+            } // Perform the random movements
+
+            for (Agent& curr_agent : agents) {
+                update_state(curr_agent);
+            } // Update state due to neighbours
+
+            step++;
+            outFile << time << "," << susceptible << "," << exposed << "," << infected << "," << recovered <<"\n";
         }
+
+        outFile.close();
     }
 }
